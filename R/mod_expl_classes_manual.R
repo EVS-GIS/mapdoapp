486af1a892ccf11830fd09c745a40f590f6f0889
JohMail <johanna.maillot@ens-lyon.fr>
2025-11-05 15:29:28 +0100

    Ajout d'indication de chargement des options de selection de classe manuelle

--- R/mod_expl_classes_manual.R
+++ R/mod_expl_classes_manual.R
@@ -22,7 +22,7 @@ mod_expl_classes_manual_ui <- function(id){
       id = ns("manual_classificationUI"),
       fluidRow(
         style = "margin-top: 10px;",
-        uiOutput(ns("metric_selectUI"))
+        uiOutput(ns("metric_selectUI"))%>% shinycssloaders::withSpinner(type = 1, caption = "Construction...")
       ),
       fluidRow(
         style = "margin-bottom: 0px; padding-bottom: 0px;",
